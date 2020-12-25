8a098d2c44988b9014043e13c59fbd02e6371afa
Emil Fresk <emil.fresk@gmail.com>
2020-12-25 16:01:18 +0100

    focusmaster

--- config.def.h
+++ config.def.h
@@ -80,6 +80,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_b,      incnmaster,     {.i = -1 } },
+	{ MODKEY|ControlMask,           XK_space,  focusmaster,    {0} },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
