9e4bd7c44dc7c26e523b687a7a7a9e2f3666d5df
Emil Fresk <emil.fresk@gmail.com>
2020-12-26 12:56:20 +0100

    thunar float

--- config.def.h
+++ config.def.h
@@ -36,6 +36,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	{ "Thunar",     NULL,       NULL,       0,            1,           -1 },
     // { "firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
