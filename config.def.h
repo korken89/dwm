7c8a8da24499a67c8933c201a1267a923f638a3c
Emil Fresk <emil.fresk@gmail.com>
2020-12-31 13:41:19 +0100

    focus does not follow mouse unless clicked

--- config.def.h
+++ config.def.h
@@ -13,6 +13,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int focusonwheel       = 0;
 static const char *fonts[]          = { "Fira Code:style=Regular:size=11" };
 static const char dmenufont[]       = "Fira Code:style=Regular:size=11";
 static const char col_gray1[]       = "#222222";
